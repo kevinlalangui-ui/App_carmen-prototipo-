c842913d6dff1b62c092b8c7708eaa788a247123
Kevin <kevin.dayan.lalangui@estudiante.epsum.school>
2026-04-08 11:30:17 +0200

    v1_App_Carmen

---
+++ src/app/layouts/main-layout/components/add-member/add-member.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-member',
+  imports: [],
+  templateUrl: './add-member.html',
+  styleUrl: './add-member.scss',
+})
+export class AddMember {}
